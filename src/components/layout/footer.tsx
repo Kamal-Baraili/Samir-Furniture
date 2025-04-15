935506f127d40ce29b2993e210de2544209ca3c2
Kamal-Baraili <kamalbaraili86@gmail.com>
2025-04-15 19:10:16 +0545

    Responsive footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <>
-      <div className=" bg-amber-50 w-full px-20 py-20 mx-auto h-auto grid md:grid-cols-9">
-        <div className="grid xl:gap-50  sm:grid-cols-6 col-span-9 sm:col-span-6">
+      <div className=" bg-amber-50 w-full px-20 py-20 mx-auto h-auto grid lg:grid-cols-9">
+        <div className="grid xl:gap-50  md:grid-cols-6 col-span-9 md:col-span-6">
           <div className="h-[200px] col-span-2">
             <img className="h-14" src="/samir-logo.png" alt="" />
           </div>
@@ -21,19 +21,19 @@ const Footer = () => {
             ))}
           </div>
         </div>
-        <div className="col-span-9 md:col-span-3 flex justify-center items-center md:block">
+        <div className="col-span-9 lg:col-span-3 flex justify-center items-center lg:block">
           <div className="h-[200px] mt-10 md:mt-0">
             <h3 className="text-[22px] text-center md:text-left p-0 m-0">
               Join Our Newsletter
             </h3>
 
-            <div className="mt-10 bg-[#13124F] rounded-3xl flex justify-between">
+            <div className="mt-10 bg-[#13124F] rounded-4xl flex justify-between">
               <input
                 className="w-[35%] lg:w-[32%] ml-5 bg-transparent outline-none text-sm lg:text-base"
                 type="text"
                 placeholder="Enter Email"
               />
-              <button className="w-[50%] text-white text-[12px] lg:text-base px-[40px] py-[15px] bg-[#9090DB] rounded-[83px] hover:bg-[#7777c8] hover:text-black hover:ease-in-out">
+              <button className="w-[50%] text-white text-[12px] lg:text-base px-[40px] py-[15px] bg-[#9090DB] rounded-4xl hover:bg-[#7777c8] hover:text-black hover:ease-in-out">
                 SUBMIT
               </button>
             </div>
